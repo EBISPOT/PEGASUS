ac05e6099d3f169e6c9a25dc3152383976f4c06f
jiyue1214 <jiyue1214@gmail.com>
2025-11-24 11:18:55 +0000

    add docs

--- peg-standard/docusaurus.config.ts
+++ peg-standard/docusaurus.config.ts
@@ -45,7 +45,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/EBISPOT/PEGASUS/tree/main/peg-standard/',
+            'https://github.com/EBISPOT/PEGASUS/tree/main/peg-standard/docs/',
         },
         blog: {
           showReadingTime: true,
